6f8e37bfc622c608c68522266472c6aa0d691a43
Your Name <thomassimon@Thomass-MacBook-Pro.local>
2020-11-24 23:32:13 -0600

    added config

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", 2);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Old Town Burger", 3);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Guys Burger", 0);
